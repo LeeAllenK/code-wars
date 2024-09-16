08926259eedb0e08a5e0b9086bf082093e4c3547
Kenny Allen <105110951+LeeAllenK@users.noreply.github.com>
2024-09-16 11:08:03 -0500

    Delete javascript directory

--- javascript/8kyu/grasshopper-function-syntax-debugging.js
+++
@@ -1,9 +0,0 @@
-/*Instructions
-Grasshopper - Function syntax debugging
-A student was working on a function and made some syntax mistakes while coding. Help them find their mistakes and fix them.
- */
-
-//Solution
-function main(verb, noun) {
-	return verb + "" + noun;
-}
